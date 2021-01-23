e80871256c083bd91e4d28f353add593b694ca14
morayodev <70142247+morayodev@users.noreply.github.com>
2021-01-23 21:10:52 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+document.getElementById("group2").addEventListener("click", myFunction);
+
+function myFunction() {
+     document.getElementById("group").style.display = "block";
+    document.getElementById("contact").style.display = "none";
+       document.getElementById("addbtn").style.display = "none";
+}
+document.getElementById("contact2").addEventListener("click", myFunctio);
+
+function myFunctio() {
+  document.getElementById("group").style.display = "none";
+    document.getElementById("contact").style.display = "block";
+    document.getElementById("addbtn").style.display = "block";
+}
